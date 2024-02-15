36f034e790963ed3bd012c221cee0825be197c8e
shruti garg <hellocoders8213@gmail.com>
2024-02-15 17:40:53 +0530

    input and btn to send message in live chat

--- src/Components/LiveChat.js
+++ src/Components/LiveChat.js
@@ -1,9 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../Utils/chatSlice";
 import { generateRandomName, randomMessage } from "../Utils/helper";
-
+import { IoMdSend } from "react-icons/io";
 const LiveChat = () => {
 	const dispatch = useDispatch();
 	const chatMessages = useSelector((store) => store.chat.messages);
@@ -24,11 +25,25 @@ const LiveChat = () => {
 		};
 	}, []);
 	return (
-		<div className="m-2 p-2 border border-white w-full h-[450px] rounded-2xl overflow-y-scroll hidden-scrollbar flex flex-col-reverse">
-			{chatMessages.map((msg, index) => (
-				<ChatMessage key={index} name={msg.name} message={msg.message} />
-			))}
-		</div>
+		<>
+			<div className="m-2 p-2 border border-white w-full h-[450px] rounded-2xl overflow-y-scroll hidden-scrollbar flex flex-col-reverse">
+				<div>
+					{chatMessages.map((msg, index) => (
+						<ChatMessage key={index} name={msg.name} message={msg.message} />
+					))}
+				</div>
+			</div>
+			<div className="w-full flex items-center mt-4 mx-2 gap-2">
+				<input
+					className="bg-[#272727] border-0 rounded-full text-gray-500 px-3 text-[15px] py-1 w-full"
+					type="text"
+					placeholder="Chat..."
+				/>
+				<div className="text-[#353434] text-2xl">
+					<IoMdSend />
+				</div>
+			</div>
+		</>
 	);
 };
 
--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,8 +1,6 @@
 const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
-export const LIVE_CHAT_COUNT = 10;
-
-
+export const LIVE_CHAT_COUNT = 25;
 
 export const YOUTUBE_VIDEO_API =
 	"https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +

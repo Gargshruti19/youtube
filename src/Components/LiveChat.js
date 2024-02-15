43ad4092d22a48678c47ef77a924e9f95c2b060b
shruti garg <hellocoders8213@gmail.com>
2024-02-15 17:52:15 +0530

    input message send to the live chat

--- src/Components/LiveChat.js
+++ src/Components/LiveChat.js
@@ -1,11 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../Utils/chatSlice";
 import { generateRandomName, randomMessage } from "../Utils/helper";
 import { IoMdSend } from "react-icons/io";
 const LiveChat = () => {
+	const [liveMessage, setLiveMessage] = useState("");
 	const dispatch = useDispatch();
 	const chatMessages = useSelector((store) => store.chat.messages);
 	console.log(chatMessages);
@@ -33,16 +34,32 @@ const LiveChat = () => {
 					))}
 				</div>
 			</div>
-			<div className="w-full flex items-center mt-4 mx-2 gap-2">
+			<form
+				className="w-full flex items-center mt-4 mx-2 gap-2"
+				onSubmit={(e) => {
+					e.preventDefault();
+					dispatch(
+						addMessage({
+							name: "Shruti Garg",
+							message: liveMessage,
+						})
+					);
+					setLiveMessage("");
+				}}
+			>
 				<input
-					className="bg-[#272727] border-0 rounded-full text-gray-500 px-3 text-[15px] py-1 w-full"
+					className="bg-[#272727] border-0 rounded-full text-white px-3 text-[15px] py-1 w-full"
 					type="text"
+					value={liveMessage}
 					placeholder="Chat..."
+					onChange={(e) => {
+						setLiveMessage(e.target.value);
+					}}
 				/>
-				<div className="text-[#353434] text-2xl">
+				<button className="text-[#353434] text-2xl">
 					<IoMdSend />
-				</div>
-			</div>
+				</button>
+			</form>
 		</>
 	);
 };
